e8acdc87c4d467b7c71b56f4ca4929102ca1d1c8
Joseph Mate <mate.joseph@gmail.com>
2020-05-23 12:06:44 -0400

    generated black keys on separate row. need to figure out alignment

--- dist/piano.js
+++ dist/piano.js
@@ -12,14 +12,30 @@ function isNextNoteSharp(notes, index) {
     return false;
 }
 var piano = document.getElementById("piano");
+// Example starting from middle C4
+// Cell Splits | | | | | | | | | | | | |  |
+// Row 1:      |   | C#| |D# |       | F#|
+// Row 2:      | C   |  D  | E   | F   |
 var blackKeys = document.createElement("tr");
+pianoNotes
+    .filter(function (pianoNote) { return pianoNote.note.isSharp; })
+    .forEach(function (pianoNote, index) {
+    var noteCell = document.createElement("td");
+    var notePlayButton = document.createElement("div");
+    notePlayButton.setAttribute("style", "color:white; background-color:black");
+    notePlayButton.innerHTML = displayName(pianoNote);
+    noteCell.appendChild(notePlayButton);
+    blackKeys.appendChild(noteCell);
+});
+piano.appendChild(blackKeys);
 var whiteKeys = document.createElement("tr");
-pianoNotes.forEach(function (pianoNote, index) {
+pianoNotes
+    .filter(function (pianoNote) { return !pianoNote.note.isSharp; })
+    .forEach(function (pianoNote, index) {
     var noteCell = document.createElement("td");
     var notePlayButton = document.createElement("div");
     notePlayButton.innerHTML = displayName(pianoNote);
     noteCell.appendChild(notePlayButton);
     whiteKeys.appendChild(noteCell);
 });
-piano.appendChild(blackKeys);
 piano.appendChild(whiteKeys);
--- piano.ts
+++ piano.ts
@@ -20,14 +20,32 @@ function isNextNoteSharp(notes: Array<AbsoluteNote>, index: number): boolean {
 
 let piano = document.getElementById("piano");
 
+// Example starting from middle C4
+// Cell Splits | | | | | | | | | | | | |  |
+// Row 1:      |   | C#| |D# |       | F#|
+// Row 2:      | C   |  D  | E   | F   |
+
 let blackKeys = document.createElement("tr");
-let whiteKeys = document.createElement("tr");
-pianoNotes.forEach((pianoNote, index) => {
-    let noteCell = document.createElement("td");
-    let notePlayButton = document.createElement("div");
-    notePlayButton.innerHTML = displayName(pianoNote);
-    noteCell.appendChild(notePlayButton);
-    whiteKeys.appendChild(noteCell);
-});
+pianoNotes
+    .filter(pianoNote => pianoNote.note.isSharp)
+    .forEach((pianoNote, index) => {
+        let noteCell = document.createElement("td");
+        let notePlayButton = document.createElement("div");
+        notePlayButton.setAttribute("style",  "color:white; background-color:black");
+        notePlayButton.innerHTML = displayName(pianoNote);
+        noteCell.appendChild(notePlayButton);
+        blackKeys.appendChild(noteCell);
+    });
 piano.appendChild(blackKeys);
+
+let whiteKeys = document.createElement("tr");
+pianoNotes
+    .filter(pianoNote => !pianoNote.note.isSharp)
+    .forEach((pianoNote, index) => {
+        let noteCell = document.createElement("td");
+        let notePlayButton = document.createElement("div");
+        notePlayButton.innerHTML = displayName(pianoNote);
+        noteCell.appendChild(notePlayButton);
+        whiteKeys.appendChild(noteCell);
+    });
 piano.appendChild(whiteKeys);

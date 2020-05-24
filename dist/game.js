ed7db979dc76d79bbb333ad33546269c27254a46
Joseph Mate <mate.joseph@gmail.com>
2020-05-24 17:48:11 -0400

    first version is ready

--- dist/game.js
+++ dist/game.js
@@ -26,3 +26,40 @@ var currentRandomNote = randomNote();
 function playRandomNote() {
     new Audio(currentRandomNote.filepath).play();
 }
+function isSame(note, randomNote) {
+    return randomNote.absoluteNote.note.rank === note.rank;
+}
+function clearStatuses() {
+    var statusSpans = document.getElementsByClassName("guessStatus");
+    for (var i = 0; i < statusSpans.length; i++) {
+        var statusSpan = statusSpans[i];
+        statusSpan.innerHTML = "";
+    }
+}
+function guess(note, statusSpan) {
+    if (isSame(note, currentRandomNote)) {
+        statusSpan.innerText = ", was correct!";
+        var topStatusSpan = document.getElementById("statusSpan");
+        topStatusSpan.innerText = "Previous note was " + displayName(currentRandomNote.absoluteNote);
+        currentRandomNote = randomNote();
+        playRandomNote();
+        setTimeout(function () {
+            clearStatuses();
+        }, 2000);
+    }
+    else {
+        statusSpan.innerText = ", is not the correct note.";
+    }
+}
+var guesses = document.getElementById("guesses");
+notesByRank.forEach(function (note) {
+    var listItem = document.createElement("li");
+    var button = document.createElement("button");
+    var statusSpan = document.createElement("span");
+    statusSpan.className = "guessStatus";
+    button.innerText = note.name;
+    button.addEventListener("click", function (e) { return guess(note, statusSpan); });
+    listItem.appendChild(button);
+    listItem.appendChild(statusSpan);
+    guesses.appendChild(listItem);
+});
--- game.ts
+++ game.ts
@@ -27,3 +27,43 @@ var currentRandomNote = randomNote();
 function playRandomNote() {
     new Audio(currentRandomNote.filepath).play();
 }
+
+function isSame(note: Note, randomNote: RandomNote): boolean {
+    return randomNote.absoluteNote.note.rank === note.rank;
+}
+
+function clearStatuses() {
+    let statusSpans = document.getElementsByClassName("guessStatus");
+    for(let i = 0; i < statusSpans.length; i++) {
+        let statusSpan = statusSpans[i];
+        statusSpan.innerHTML = "";
+    }
+}
+
+function guess(note: Note, statusSpan: HTMLElement) {
+    if(isSame(note, currentRandomNote)) {
+        statusSpan.innerText = ", was correct!";
+        var topStatusSpan = document.getElementById("statusSpan");
+        topStatusSpan.innerText = "Previous note was " + displayName(currentRandomNote.absoluteNote);
+        currentRandomNote = randomNote();
+        playRandomNote();
+        setTimeout(() => {
+            clearStatuses();
+        }, 2000);
+    } else {
+        statusSpan.innerText = ", is not the correct note.";
+    }
+}
+
+let guesses = document.getElementById("guesses");
+notesByRank.forEach(note => {
+    let listItem = document.createElement("li");
+    let button = document.createElement("button");
+    let statusSpan = document.createElement("span");
+    statusSpan.className = "guessStatus";
+    button.innerText = note.name;
+    button.addEventListener("click", (e:Event) => guess(note, statusSpan));
+    listItem.appendChild(button);
+    listItem.appendChild(statusSpan);
+    guesses.appendChild(listItem);
+});

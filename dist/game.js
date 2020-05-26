c3fcaf2fc57fa1711262616b386c3662a4bce184
Joseph Mate <mate.joseph@gmail.com>
2020-05-25 22:02:04 -0400

    free practice mode complete

--- dist/game.js
+++ dist/game.js
@@ -8,24 +8,6 @@ var RandomNote = /** @class */ (function () {
 function randomInteger(lowerBound, upperbound) {
     return Math.floor(Math.random() * (upperbound - lowerBound)) + lowerBound;
 }
-function randomNote() {
-    var totalNotes = guitarNotes.length + pianoNotes.length;
-    var randomIndex = randomInteger(0, totalNotes - 1);
-    if (randomIndex < guitarNotes.length) {
-        var guitarIndex = randomIndex;
-        var guitarNote = guitarNotes[guitarIndex];
-        return new RandomNote(getGuitarFilepath(guitarNote), guitarNote.absoluteNote);
-    }
-    else {
-        var pianoIndex = randomIndex - guitarNotes.length;
-        var pianoNote = pianoNotes[pianoIndex];
-        return new RandomNote(getPianoFilepath(pianoNote), pianoNote);
-    }
-}
-var currentRandomNote = randomNote();
-function playRandomNote() {
-    new Audio(currentRandomNote.filepath).play();
-}
 function isSame(note, randomNote) {
     return randomNote.absoluteNote.note.rank === note.rank;
 }
@@ -35,6 +17,83 @@ function clearStatuses() {
         var statusSpan = statusSpans[i];
         statusSpan.innerHTML = "";
     }
+}
+// map of note rank to the checkbox that enables it
+var freeModeNoteSettings = [];
+// prepare the guess settings
+var freeModeSettingsList = document.getElementById("freeModeSettingsList");
+notesByRank.forEach(function (note) {
+    var listItem = document.createElement("li");
+    var check = document.createElement("input");
+    check.type = "checkbox";
+    check.checked = true;
+    freeModeNoteSettings[note.rank] = check;
+    listItem.appendChild(check);
+    var span = document.createElement("span");
+    span.innerText = note.name;
+    listItem.appendChild(span);
+    freeModeSettingsList.appendChild(listItem);
+});
+function getEnabledNotes() {
+    var result = [];
+    freeModeNoteSettings.forEach(function (value, index) {
+        if (value.checked) {
+            result.push(notesByRank[index]);
+        }
+    });
+    return result;
+}
+function getNotesByRankEnabledMap() {
+    var enabledNotes = {};
+    freeModeNoteSettings.forEach(function (value, index) {
+        if (value.checked) {
+            enabledNotes[index] = true;
+        }
+        else {
+            false;
+        }
+    });
+    return enabledNotes;
+}
+function getFilteredGuitarNotes() {
+    var enabledNotes = getNotesByRankEnabledMap();
+    var result = [];
+    guitarNotes.forEach(function (guitarNote) {
+        if (enabledNotes[guitarNote.absoluteNote.note.rank]) {
+            result.push(guitarNote);
+        }
+    });
+    return result;
+}
+function getFilteredPianoNotes() {
+    var enabledNotes = getNotesByRankEnabledMap();
+    var result = [];
+    pianoNotes.forEach(function (pianoNote) {
+        if (enabledNotes[pianoNote.note.rank]) {
+            result.push(pianoNote);
+        }
+    });
+    return result;
+}
+function randomNote() {
+    var filteredGuitarNotes = getFilteredGuitarNotes();
+    var filteredPianoNotes = getFilteredPianoNotes();
+    var totalNotes = filteredGuitarNotes.length + filteredPianoNotes.length;
+    var randomIndex = randomInteger(0, totalNotes - 1);
+    if (randomIndex < filteredGuitarNotes.length) {
+        var guitarIndex = randomIndex;
+        var guitarNote = filteredGuitarNotes[guitarIndex];
+        return new RandomNote(getGuitarFilepath(guitarNote), guitarNote.absoluteNote);
+    }
+    else {
+        var pianoIndex = randomIndex - filteredGuitarNotes.length;
+        var pianoNote = filteredPianoNotes[pianoIndex];
+        return new RandomNote(getPianoFilepath(pianoNote), pianoNote);
+    }
+}
+var currentRandomNote = randomNote();
+function playRandomNote() {
+    new Audio(currentRandomNote.filepath).play();
 }
 function guess(note, statusSpan) {
     if (isSame(note, currentRandomNote)) {
@@ -52,14 +111,20 @@ function guess(note, statusSpan) {
     }
 }
 var guesses = document.getElementById("guesses");
-notesByRank.forEach(function (note) {
-    var listItem = document.createElement("li");
-    var button = document.createElement("button");
-    var statusSpan = document.createElement("span");
-    statusSpan.className = "guessStatus";
-    button.innerText = note.name;
-    button.addEventListener("click", function (e) { return guess(note, statusSpan); });
-    listItem.appendChild(button);
-    listItem.appendChild(statusSpan);
-    guesses.appendChild(listItem);
-});
+function applyFreeModeSettings() {
+    guesses.innerHTML = "";
+    getEnabledNotes().forEach(function (note) {
+        var listItem = document.createElement("li");
+        var button = document.createElement("button");
+        var statusSpan = document.createElement("span");
+        statusSpan.className = "guessStatus";
+        button.innerText = note.name;
+        button.addEventListener("click", function (e) { return guess(note, statusSpan); });
+        listItem.appendChild(button);
+        listItem.appendChild(statusSpan);
+        guesses.appendChild(listItem);
+    });
+    currentRandomNote = randomNote();
+}
+// prepare the guess buttons
+applyFreeModeSettings();
--- game.ts
+++ game.ts
@@ -1,31 +1,10 @@
-
-
 class RandomNote {
     constructor(public filepath: string, public absoluteNote: AbsoluteNote) { }
 }
 
+
 function randomInteger(lowerBound: number, upperbound: number): number {
     return Math.floor(Math.random() * (upperbound-lowerBound)) + lowerBound;
-}
-
-function randomNote(): RandomNote {
-    let totalNotes = guitarNotes.length + pianoNotes.length;
-    let randomIndex = randomInteger(0, totalNotes-1);
-    if(randomIndex < guitarNotes.length) {
-        let guitarIndex = randomIndex;
-        let guitarNote = guitarNotes[guitarIndex];
-        return new RandomNote(getGuitarFilepath(guitarNote), guitarNote.absoluteNote);
-    } else {
-        let pianoIndex = randomIndex - guitarNotes.length;
-        let pianoNote = pianoNotes[pianoIndex]
-        return new RandomNote(getPianoFilepath(pianoNote), pianoNote);
-    }
-}
-
-var currentRandomNote = randomNote();
-
-function playRandomNote() {
-    new Audio(currentRandomNote.filepath).play();
 }
 
 function isSame(note: Note, randomNote: RandomNote): boolean {
@@ -38,6 +17,93 @@ function clearStatuses() {
         let statusSpan = statusSpans[i];
         statusSpan.innerHTML = "";
     }
+}
+
+// map of note rank to the checkbox that enables it
+let freeModeNoteSettings = [];
+// prepare the guess settings
+let freeModeSettingsList = document.getElementById("freeModeSettingsList");
+notesByRank.forEach(note => {
+    let listItem = document.createElement("li");
+    let check = document.createElement("input");
+    check.type = "checkbox";
+    check.checked = true;
+    freeModeNoteSettings[note.rank] = check;
+    listItem.appendChild(check);
+    let span = document.createElement("span");
+    span.innerText = note.name;
+    listItem.appendChild(span);
+    freeModeSettingsList.appendChild(listItem);
+});
+
+function getEnabledNotes(): Array<Note> {
+    let result = [];
+
+    freeModeNoteSettings.forEach((value, index) => {
+        if (value.checked) {
+            result.push(notesByRank[index]);
+        }
+    });
+
+    return result;
+}
+
+function getNotesByRankEnabledMap() {
+    let enabledNotes = {};
+    freeModeNoteSettings.forEach((value, index) => {
+        if (value.checked) {
+            enabledNotes[index] = true;
+        } else {
+            false;
+        }
+    });
+    return enabledNotes;
+}
+
+function getFilteredGuitarNotes(): Array<GuitarNote> {
+    let enabledNotes = getNotesByRankEnabledMap();
+
+    let result = [];
+    guitarNotes.forEach(guitarNote => {
+        if(enabledNotes[guitarNote.absoluteNote.note.rank]) {
+            result.push(guitarNote);
+        }
+    });
+    return result;
+}
+
+function getFilteredPianoNotes(): Array<AbsoluteNote> {
+    let enabledNotes = getNotesByRankEnabledMap();
+
+    let result = [];
+    pianoNotes.forEach(pianoNote => {
+        if(enabledNotes[pianoNote.note.rank]) {
+            result.push(pianoNote);
+        }
+    });
+    return result;
+}
+
+function randomNote(): RandomNote {
+    let filteredGuitarNotes = getFilteredGuitarNotes();
+    let filteredPianoNotes = getFilteredPianoNotes();
+
+    let totalNotes = filteredGuitarNotes.length + filteredPianoNotes.length;
+    let randomIndex = randomInteger(0, totalNotes-1);
+    if(randomIndex < filteredGuitarNotes.length) {
+        let guitarIndex = randomIndex;
+        let guitarNote = filteredGuitarNotes[guitarIndex];
+        return new RandomNote(getGuitarFilepath(guitarNote), guitarNote.absoluteNote);
+    } else {
+        let pianoIndex = randomIndex - filteredGuitarNotes.length;
+        let pianoNote = filteredPianoNotes[pianoIndex]
+        return new RandomNote(getPianoFilepath(pianoNote), pianoNote);
+    }
+}
+
+var currentRandomNote = randomNote();
+function playRandomNote() {
+    new Audio(currentRandomNote.filepath).play();
 }
 
 function guess(note: Note, statusSpan: HTMLElement) {
@@ -56,14 +122,21 @@ function guess(note: Note, statusSpan: HTMLElement) {
 }
 
 let guesses = document.getElementById("guesses");
-notesByRank.forEach(note => {
-    let listItem = document.createElement("li");
-    let button = document.createElement("button");
-    let statusSpan = document.createElement("span");
-    statusSpan.className = "guessStatus";
-    button.innerText = note.name;
-    button.addEventListener("click", (e:Event) => guess(note, statusSpan));
-    listItem.appendChild(button);
-    listItem.appendChild(statusSpan);
-    guesses.appendChild(listItem);
-});
+function applyFreeModeSettings() {
+    guesses.innerHTML = "";
+    getEnabledNotes().forEach(note => {
+        let listItem = document.createElement("li");
+        let button = document.createElement("button");
+        let statusSpan = document.createElement("span");
+        statusSpan.className = "guessStatus";
+        button.innerText = note.name;
+        button.addEventListener("click", (e:Event) => guess(note, statusSpan));
+        listItem.appendChild(button);
+        listItem.appendChild(statusSpan);
+        guesses.appendChild(listItem);
+    });
+    currentRandomNote = randomNote();
+}
+
+// prepare the guess buttons
+applyFreeModeSettings();
